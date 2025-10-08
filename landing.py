c623514a5cba1e3482827d57ae60c63b79de0347
Gryffin T <Gryffintubon@gmail.com>
2025-10-08 00:10:08 -0500

    Update landing.py

--- landing.py
+++ landing.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image, ImageOps, ImageDraw
 import base64
 from pathlib import Path
-import streamlit.components.v1 as components
 
 def landing_page():
     BASE_DIR = Path(__file__).parent
@@ -33,7 +32,7 @@ def landing_page():
 
 
     # CSS to create frosted glass effect top bar
-    components.html("""
+    st.html("""
     <style>
     .css-18e3th9 {
         padding: 0 !important;
