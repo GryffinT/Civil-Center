441984f2043b3ee195cb8f901f0c25ea441eacd9
Gryffin T <Gryffintubon@gmail.com>
2025-10-08 15:24:48 -0500

    Update login.py

--- login.py
+++ login.py
@@ -5,19 +5,27 @@ if "page" not in st.session_state:
     st.session_state.page = 1
 
 def login_page(): 
-    with st.form("my_form"):
-        tab1, tab2 = st.tabs(["Login", "Signup"])
-        with tab1:
-            login_username = ""
-            st.header("Login")
-            login_username = st.text_input(key="lu", label="Username")
-            st.text_input(key="lp", label="Password")
-        with tab2:
-            signup_username = ""
-            st.header("Signup")
-            signup_username = st.text_input(key="su", label="Username")
-            st.text_input(key="sp", label="Password")
-        submitted = st.form_submit_button(key="register", label="Submit")
-    if submitted:
-        st.session_state.page = 0
-        st.rerun()
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+    
+    with col2:
+        with st.form("my_form"):
+            tab1, tab2 = st.tabs(["Login", "Signup"])
+            with tab1:
+                login_username = ""
+                st.header("Login")
+                login_username = st.text_input(key="lu", label="Username")
+                st.text_input(key="lp", label="Password")
+            with tab2:
+                signup_username = ""
+                st.header("Signup")
+                signup_username = st.text_input(key="su", label="Username")
+                st.text_input(key="sp", label="Password")
+            submitted = st.form_submit_button(key="register", label="Submit")
+        if submitted:
+            st.session_state.page = 0
+            st.rerun()
+
+    
+    with col3:
