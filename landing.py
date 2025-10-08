b1d2734b306b4f210399d67b9aa7adac09970cfa
Gryffin T <Gryffintubon@gmail.com>
2025-10-08 00:13:51 -0500

    Update landing.py

--- landing.py
+++ landing.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image, ImageOps, ImageDraw
 import base64
 from pathlib import Path
+import streamlit.components.v1 as components
+
 
 def landing_page():
     BASE_DIR = Path(__file__).parent
