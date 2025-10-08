bbe2c46426fbf6c59417875ae2e95dd58f8ab9b8
Gryffin T <Gryffintubon@gmail.com>
2025-10-08 12:13:49 -0500

    Update ui_handler.py

--- ui_handler.py
+++ ui_handler.py
@@ -1,5 +1,6 @@
 from landing import landing_page
 import streamlit as st
+from login import login_page
 
 if "page" not in st.session_state:
   st.session_state.page = 0
@@ -7,4 +8,4 @@ if "page" not in st.session_state:
 if st.session_state.page == 0:
   landing_page()
 elif st.session_state.page == 1:
-  st.write("Login")
+  login_page()
