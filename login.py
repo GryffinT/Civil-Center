d142b2c1a18792732806d9a3e6a5d79a2558b5ff
Gryffin T <Gryffintubon@gmail.com>
2025-10-08 15:11:55 -0500

    Update login.py

--- login.py
+++ login.py
@@ -6,12 +6,17 @@ if "page" not in st.session_state:
 
 def login_page(): 
     with st.form("my_form"):
-        tab1, tab2 = st.tabs(["Cat", "Dog"])
+        tab1, tab2 = st.tabs(["Login", "Signup"])
         
         with tab1:
             st.header("Login")
+            st.text_input("Username")
+            st.text_input("Password")
         with tab2:
             st.header("Signup")
+            st.text_input("Username")
+            st.text_input("Password")
         submitted = st.form_submit_button()
     if submitted:
         st.session_state.page = 0
+        st.rerun()
