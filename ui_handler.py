e099ca3330ca9b1d50e134520da27ad7053c04d1
Gryffin T <Gryffintubon@gmail.com>
2025-10-09 11:49:40 -0500

    Update ui_handler.py

--- ui_handler.py
+++ ui_handler.py
@@ -4,6 +4,8 @@ from login import login_page
 from my_centers import my_centers_page
 from center import center_page
 from streamlit.components.v1 import declare_component
+from pathlib import Path
+
 
 if "page" not in st.session_state:
   st.session_state.page = 0
@@ -17,12 +19,13 @@ elif st.session_state.page == 1:
 elif st.session_state.page == 2:
   my_centers_page()
 elif st.session_state.page == 3:
-  black_button = declare_component("black_button", path="components/black_button")
+  component_path = Path(__file__).parent / "components" / "black_button"
+  black_button = declare_component("black_button", path=str(component_path))
 
-  clicked = black_button(default=False, key="main_button")
+  clicked = black_button(default=False, key="x")
 
   if clicked:
       st.success("Button clicked!")
   else:
-      st.write("Waiting for click...")
+      st.write("Waiting for click…")
   center_page(st.session_state.active_center)
