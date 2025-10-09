eba4097bd5550add71959975efd79f7ab209f51f
Gryffin T <Gryffintubon@gmail.com>
2025-10-09 11:43:19 -0500

    Update ui_handler.py

--- ui_handler.py
+++ ui_handler.py
@@ -11,16 +11,16 @@ if "active_center" not in st.session_state:
   st.session_state.active_center = None
 
 if st.session_state.page == 0:
-  black_button = declare_component("black_button", path="components/black_button/")
-
-  clicked = black_button(default=False, key="other_page_button")
-
-  if clicked:
-      st.success("Other page button clicked!")
   landing_page()
 elif st.session_state.page == 1:
   login_page()
 elif st.session_state.page == 2:
   my_centers_page()
 elif st.session_state.page == 3:
+  black_button = declare_component("black_button", path="components/black_button/")
+
+  clicked = black_button(default=False, key="other_page_button")
+
+  if clicked:
+      st.success("Other page button clicked!")
   center_page(st.session_state.active_center)
