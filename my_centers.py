18e0e26b2ebadbb75eaabff6a4562786168c874f
Gryffin T <Gryffintubon@gmail.com>
2025-10-09 11:07:46 -0500

    config testing

--- my_centers.py
+++ my_centers.py
@@ -233,7 +233,7 @@ def my_centers_page():
                                 <p style="background-color: #f0f0f0; border-radius: 5px; padding: 10px;">Description: {center.get('description', 'No description provided.')}</p>
                             </div>
                         """)
-                    if st.button("Go to Center", key=f"go_{cid_str}", color="#000000", use_container_width=True):
+                    if st.button("Go to Center", key=f"go_{cid_str}", use_container_width=True, type="tertiary"):
                         st.session_state.active_center = cid
                         st.session_state.page = 3
                         st.rerun()
