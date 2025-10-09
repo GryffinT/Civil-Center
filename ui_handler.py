ed35547b03f377b25c2b762dc0bc7544ad42e0f5
Gryffin T <Gryffintubon@gmail.com>
2025-10-09 00:19:07 -0500

    Update ui_handler.py

--- ui_handler.py
+++ ui_handler.py
@@ -5,6 +5,8 @@ from my_centers import my_centers_page
 
 if "page" not in st.session_state:
   st.session_state.page = 0
+if "active_center" not in st.session_state:
+  st.session_state.active_center = None
 
 if st.session_state.page == 0:
   landing_page()
@@ -12,3 +14,5 @@ elif st.session_state.page == 1:
   login_page()
 elif st.session_state.page == 2:
   my_centers_page()
+  if st.session_state.active_center != None:
+    st.write(f"Active Center ID: {st.session_state.active_center}")
