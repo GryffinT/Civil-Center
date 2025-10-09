620faa049de24bac61948559c380d6a598da9351
Gryffin T <Gryffintubon@gmail.com>
2025-10-09 11:40:30 -0500

    component?

--- ui_handler.py
+++ ui_handler.py
@@ -3,6 +3,7 @@ import streamlit as st
 from login import login_page
 from my_centers import my_centers_page
 from center import center_page
+from st.components.v1 import declare_component
 
 if "page" not in st.session_state:
   st.session_state.page = 0
@@ -10,6 +11,13 @@ if "active_center" not in st.session_state:
   st.session_state.active_center = None
 
 if st.session_state.page == 0:
+  # Reuse the same component path
+  black_button = declare_component("black_button", path="components/black_button")
+
+  clicked = black_button(default=False, key="other_page_button")
+
+  if clicked:
+      st.success("Other page button clicked!")
   landing_page()
 elif st.session_state.page == 1:
   login_page()
