efef9a343c0ba2240b6f4010bcf599bf211c195e
Gryffin T <Gryffintubon@gmail.com>
2025-10-10 12:36:44 -0500

    Update center.py

--- center.py
+++ center.py
@@ -178,9 +178,9 @@ def center_page(center_id):
                         2: "Problem",
                     }
                     color_map = {
-                        0: "blue",
-                        1: "green",
-                        2: "red"
+                        0: ["blue", "#3366cc"]
+                        1: ["green", "#016e3d"]
+                        2: ["red", "#6e0101"]
                     }
                     if bad:
                         st.html(f"""
@@ -195,7 +195,7 @@ def center_page(center_id):
                                 <h2 style="margin-bottom: 0.5em;">{title}</h2>
                                 <h4 style="color: #666; margin-top: 0;">Posted by {name}</h4>
                                 <p style="overflow-wrap: break-word; white-space: pre-wrap;">{content}</p>
-                                <span style="display: inline-block; background: {color_map[bad]}; color: #fff; border-radius: 5px; padding: 0.2em 0.6em; font-size: 0.9em;">
+                                <span style="display: inline-block; font-color: {color_map[bad][0]}; background: {color_map[bad][1]}; color: #fff; border-radius: 5px; padding: 0.2em 0.6em; font-size: 0.9em;">
                                     {badge_map[bad]}
                                 </span>
                             </div>
