46f485d50dcc5a8d7b30968f82fa8b926e2cc761
Gryffin T <Gryffintubon@gmail.com>
2025-10-13 12:48:38 -0500

    Update center.py

--- center.py
+++ center.py
@@ -182,6 +182,7 @@ def center_page(center_id):
                 name = post.get("name", "Unknown")
                 content = post.get("content", "")
                 bad = post.get("tags", "")
+                st.write(len(st.session_state.semantic_post_content))
                 for entry in range(len(st.session_state.semantic_post_content)):
                     similarity = util.cos_sim(embed_text(content), embed_text(st.session_state.semantic_post_content[entry])).item()
                     st.write(similarity)
